26ca9e8cf000f84cc2eeb52d8ab8cc1815623f19
Gokul <153418048+GeniusPikachu@users.noreply.github.com>
2025-06-05 10:31:00 +0530

    Delete avatar project.py

--- avatar project.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=200)
-frm.grid()
-ttk.Label(frm, text="avatar!").grid(column=200, row=200)
-ttk.Button(frm, text="exit", command=root.destroy).grid(column=100, row=100)
-root.mainloop()
